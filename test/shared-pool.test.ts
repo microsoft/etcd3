828281870ce67cf4d028968e1af0c98136547fc8
Connor Peet <connor@peet.io>
2017-04-13 11:38:28 -0700

    Prevent fail getting hit by concurrent callers and unnecessarily backing off

--- test/shared-pool.test.ts
+++ test/shared-pool.test.ts
@@ -65,4 +65,12 @@ describe('shared pool', () => {
     clock.tick(500);
     expect(await getAll()).to.deep.equal([0, 2, 1]);
   });
+
+  it('should not back off multiple times if multiple callers fail', async () => {
+    const cnx = await pool.pull();
+    pool.fail(cnx);
+    expect((<any> pool).resources[0].availableAfter).to.equal(Date.now() + 500);
+    pool.fail(cnx);
+    expect((<any> pool).resources[0].availableAfter).to.equal(Date.now() + 500);
+  });
 });
